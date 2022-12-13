3bb6ed034e9f1d03f15e8634901a09ffac2eaad4
Emmanuel Egomson <fegomson@gmail.com>
2022-12-13 15:30:42 -0600

    changes client url

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: ['http://localhost:3000', 'https://pinventory.vercel.app'],
+    origin: ['http://localhost:3000', 'https://pinventory-nine.vercel.app'],
     credentials: true,
   })
 );
